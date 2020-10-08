d42625f4fc71b59a565a1389baf6b4d25d8d01d8
nsyawali12 <nsyawali12@gmail.com>
2020-10-09 02:24:43 +0700

    login is done

--- js/dist/login.dev.js
+++ js/dist/login.dev.js
@@ -1,1 +1,27 @@
 "use strict";
+
+console.clear();
+var loginBtn = document.getElementById('login');
+var signupBtn = document.getElementById('signup');
+loginBtn.addEventListener('click', function (e) {
+  var parent = e.target.parentNode.parentNode;
+  Array.from(e.target.parentNode.parentNode.classList).find(function (element) {
+    if (element !== "slide-up") {
+      parent.classList.add('slide-up');
+    } else {
+      signupBtn.parentNode.classList.add('slide-up');
+      parent.classList.remove('slide-up');
+    }
+  });
+});
+signupBtn.addEventListener('click', function (e) {
+  var parent = e.target.parentNode;
+  Array.from(e.target.parentNode.classList).find(function (element) {
+    if (element !== "slide-up") {
+      parent.classList.add('slide-up');
+    } else {
+      loginBtn.parentNode.parentNode.classList.add('slide-up');
+      parent.classList.remove('slide-up');
+    }
+  });
+});
